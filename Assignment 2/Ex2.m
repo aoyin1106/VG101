6757e42cd016910b3f898f3ae8305926d3e139a1
Li Shi <shili2017@sjtu.edu.cn>
2018-05-26 17:26:45 +0800

    Li Shi

---
+++ Assignment 2/Ex2.m
@@ -0,0 +1,28 @@
+clear all, clc;
+
+n = input('Input a positive integer: ');
+
+while true
+    num1 = squares(n);
+    if (isprime(n)) && (rem(n, 4) == 1) && (num1 ~= 0)
+        fprintf('Pythagorean prime: %d = %d ^ 2 + %d ^ 2 \n', n, num1, sqrt(n - num1 ^2));
+        break
+    else
+        n = n + 1;
+    end
+end
+
+function num1 = squares(num)    %Judge whether the number can be written as the sum of two squares.
+    flag = false;
+    for i = 1 : floor(sqrt(num))
+        num2 = sqrt(num - i ^ 2);
+        if (num2 == fix(num2))
+            num1 = i;
+            flag = true;
+            break;
+        end
+    end
+    if flag == false
+        num1 = 0;
+    end
+end
