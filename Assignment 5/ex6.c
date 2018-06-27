709af1e9f34b1d6c409cb79054756cd0b3d1c145
Li Shi <shili2017@sjtu.edu.cn>
2018-06-28 00:20:27 +0800

    Li Shi

--- Assignment 5/ex6.c
+++ Assignment 5/ex6.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SWAP(a,b) { a ^= b; b ^= a; a ^= b; }
+//#define TEST
+#define SWAP(a,b) { a ^= b; b ^= a; a ^= b; }                       //Swap two binary numbers using XOR
 
+int getDigit(unsigned int len, unsigned long int b);
 unsigned long int mult(unsigned long int a, unsigned long int b);
 
 int main () {
     unsigned long int a, b;
-    srand(time(NULL));
+    srand(time(NULL));                                              //Initialize rand function
 
-    #ifndef TEST
-    a = rand();
+    #ifndef TEST                                                    //No -DTEST in compiling configurations
+    a = rand();                                                     //Randomly generate two integers between 0 and RAND_MAX
     b = rand();
-    printf("%ld*%ld=%ld %ld\n", a, b, mult(a, b), RAND_MAX);
+    printf("%ld*%ld=%ld %ld\n", a, b, mult(a, b), RAND_MAX);        //Display the result of low-level multiplying, and RAND_MAX
     #endif
 
-    #ifdef TEST
+    #ifdef TEST                                                     //-DTEST in compiling configurations
     int i;
-    for(i=0; i < 1000000; i++)
+    for(i=0; i < 1000000; i++)                                      //Test 1000000 cases
     {
         a = rand();
         b = rand();
-        if (mult(a,b) != a * b)
+        if (mult(a,b) != a * b)                                     //If the program finds an error, display it and exit with exitcode -1
         {
             fprintf(stderr,"Error (%d): a=%ld, b=%ld, a*b=%ld, k(a,b)=%ld\n", i, a, b, a*b, mult(a,b));
             exit(-1);
@@ -30,20 +32,38 @@ int main () {
     #endif
 }
 
-unsigned long int mult(unsigned long int a, unsigned long int b)
+int getDigit(unsigned int len, unsigned long int b)
+{
+    if (len == 1)                                                   //The criteria for the recursion to exit.
+        return 1;                                                   //If the length of binary number is 1, return 1
+    else {
+        unsigned int halfLen = len >> 1;
+        unsigned long int left = b >> halfLen;                      //Get the left part and right part of the binary number
+        unsigned long int right = b & ((1 << halfLen) - 1);         //e.g. halfLen = 4, b = 1001 -> left = 10, right = 01
+        if (left)                                                   //Metaphysics (Xuan Xue) here
+            return getDigit(halfLen, left) + halfLen;
+        else
+            return getDigit(halfLen, right);
+    }
+}
+
+unsigned long int mult(unsigned long int a, unsigned long int b)    //Only work for non-negative integers
 {
     int i, n, N;
     unsigned long int x0, y0, z0, z1 = 1;
-    if (a < b)
+    if (a < b)                                                      //a should be greater than or equal to b
         SWAP(a,b);
-    if (b == 0)
+    if (b == 0)                                                     //The criteria for the recursion to exit
         return 0;
-    for (n = -1, i = 1; i <= b; i <<= 1, n++); /* not optimal */
-    for (N = n; i <= a; i <<= 1, N++);
+    //for (n = -1, i = 1; i <= b; i <<= 1, n++); /* not optimal */  //Get the number of digits of b and a
+    //for (N = n; i <= a; i <<= 1, N++);                            //But this number is the number of digits minus 1, e.g., b = 11010, n = 4
 
-    y0 = b & ((1 << n) - 1);
+    n = getDigit((unsigned int)sizeof(unsigned long int)*8, b) - 1; //The function realizes the same feature as above.
+    N = getDigit((unsigned int)sizeof(unsigned long int)*8, a) - 1; //However, the time complexity reduces from O(s) to O(log s), here s = 64
+
+    y0 = b & ((1 << n) - 1);                                        //Get the last n digits of b. e.g. b = 11010, y0 = 1010
     x0 = a & ((1 << N) - 1);
-    z0 = mult(x0,y0);
-    i = N + n;
-    return ((z1 << i) + (x0 << n) + (y0 << N) + z0);
+    z0 = mult(x0,y0);                                               //Recursion, calculate x0 * y0
+    i = N + n;                                                      //Total digits of x0 and y0
+    return ((z1 << i) + (x0 << n) + (y0 << N) + z0);                //Use Karatsuba algorithm to return the result
 }
