1d2c15c30a76fd1696a20c78a443c2b2de1549c4
Angelinazy <40574759+Angelinazy@users.noreply.github.com>
2018-06-28 00:57:06 +0800

    Add files via upload

--- Assignment 5/ex3.c
+++ Assignment 5/ex3.c
@@ -3,15 +3,15 @@
 
     int findstring(char *a,char *b,char *c);
     int main() {
-        char sentence[10000];
-        char string[10000];
+        char sentence[100000];
+        char string[100000];
         int all;
         printf("Input a sentence: ");
-        char* temp=fgets(sentence,10000,stdin);
+        char* temp=fgets(sentence,100000,stdin);
         if ('\n'==sentence[strlen(sentence)-1])
             sentence[strlen(sentence)-1]='\0';
         printf("Input a string: ");
-        char* temp1=fgets(string,10000,stdin);
+        char* temp1=fgets(string,100000,stdin);
         if ('\n'==string[strlen(string)-1])
             string[strlen(string)-1]='\0';
         all=findstring(temp,temp1,&sentence[strlen(sentence)]);
