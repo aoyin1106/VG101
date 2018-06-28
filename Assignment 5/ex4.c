74e1e923c42f81244d07e40f945073c934d7de15
Adia-wty <39758262+Adia-wty@users.noreply.github.com>
2018-06-28 10:02:28 +0800

    Update ex4.c

--- Assignment 5/ex4.c
+++ Assignment 5/ex4.c
@@ -2,38 +2,37 @@
 #include <math.h>
 #include <float.h>
 
-double solve(double a, double b, double c, double range_min, double range_max);
-double f(double a, double b, double c, double x);
-
-int main()
-{
-    double v0, angle, h0, range_min, range_max;
-    scanf("%lf %lf %lf %lf %lf", &v0, &angle, &h0, &range_min, &range_max);
-    angle = angle / 180 * 3.1415926536;     //transform angle from degree into radian.
-    double c = h0, b = tan(angle), a = -(9.81/(2.0 * v0 * v0 * cos(angle) * cos(angle)));
-                                            //simplify the function.
-    double ans = solve(a,b,c,range_min,range_max);
-    printf("%.6lf", ans);
-    return 0;
+double trajectory(double a,double b,double c,double average){
+    return a * pow(average,2) + b * average + c;
 }
 
-// Use bisection to get the solution of the quadratic equation.
-double solve(double a, double b, double c, double range_min, double range_max)
-{
-    double mid = (range_max + range_min) / 2;
-    if (range_max - range_min < FLT_EPSILON)
-        return mid;
-    else
+double bisection(double a,double b,double c,double min,double max) {
+    double average = (min + max) / 2.0;
+
+    if (max - min < FLT_EPSILON) // if the range is small enough
     {
-        if (f(a,b,c,range_min) * f(a,b,c,mid) <= 0)
-            return solve(a,b,c,range_min,mid);
+        return average;
+    } else {
+        if (trajectory(a, b, c, average) * trajectory(a, b, c, min) <= 0)
+        { max=average; }
         else
-            return solve(a,b,c,mid,range_max);
+            { min=average; }
     }
+    return bisection(a,b,c,min,max);
 }
 
-//A simple quadratic function, used to simplify the expression in "solve" function.
-double f(double a, double b, double c, double x)
-{
-    return a * x * x + b * x + c;
+
+int main() {
+    double height,min,max,v,theta;
+    scanf("%lf %lf %lf %lf %lf",&v,&theta,&height,&min,&max);
+
+    theta=theta/180.0*3.1415926536;   // convert angles in radians
+            // the following only need to be calculated once in the whole program
+    double a = -(9.81 / (2.0 * pow(v * cos(theta),2)));
+    double b = tan(theta);
+    double c = height;
+
+    double ans = bisection(a,b,c,min,max);
+    printf("%.6lf",ans);
+    return 0;
 }
